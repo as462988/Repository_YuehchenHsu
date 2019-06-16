c62d1bc3593bc3bfaf6f3e4095148471c656a32c
HSU,Yuehx-chen <as462988@gmail.com>
2019-06-16 15:24:26 +0800

    Update Contents.swift

--- week1/Assignmemt4.playground/Contents.swift
+++ week1/Assignmemt4.playground/Contents.swift
@@ -6,15 +6,15 @@ func greet(person name: String) -> String{
 greet(person: "Celeste")
 
 //2.
-func multiply(a:Int, b:Int = 10) -> Void{
- print(a * b)
+func multiply(a: Int, b: Int = 10) -> Void{
+ print( a * b )
 }
-multiply(a: 22)
+multiply( a: 22)
 
-//3.
-//Argument label為參數在Function外部呼叫時使用的名稱;Parameter label為參數在Function內部使用的名稱
-//如果兩個都有被定義，在內部只能使用Parameter label
-//沒有定義Argument label時，在外部呼叫時就使用Parameter label。
+/*3.
+Argument label為參數在Function外部呼叫時使用的名稱;Parameter label為參數在Function內部使用的名稱
+如果兩個都有被定義，在內部只能使用Parameter label
+ 沒有定義Argument label時，在外部呼叫時就使用Parameter label。*/
 
 //4.
 func birthday() -> String {
