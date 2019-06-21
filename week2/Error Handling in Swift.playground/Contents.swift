561e3786298f4a58dc2f8475deb9502c0a6f54fa
HSU,Yuehx-chen <as462988@gmail.com>
2019-06-21 15:48:44 +0800

    done of assignment

--- week2/Error Handling in Swift.playground/Contents.swift
+++ week2/Error Handling in Swift.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
 
-var str = "Hello, playground"
+enum GuessNumberGameError: Error {
+    case wrongNumber
+}
+
+class GuessNumerGame {
+    var targeNumber = 10
+    
+    func guess(number: Int) throws {
+        guard number == targeNumber else {
+        throw GuessNumberGameError.wrongNumber
+
+        }
+        print("Guess the right number: \(targeNumber)")
+    }
+}
+
+let someGame = GuessNumerGame()
+//try someGame.guess(number: 20)
+do{
+    try someGame.guess(number: 20)
+}catch let error {
+    print("This targeNumber isn't correct! ")
+}
+
+
+
+
+
+
+
+
+
+
--- week2/Protocol in Swift.playground/Contents.swift
+++ week2/Protocol in Swift.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
+//1.
+protocol PoliceMan{
+    func arrestCriminals()
+}
 
-var str = "Hello, playground"
+//2.4.
+struct Person: PoliceMan {
+    let name: String
+    let toolMan: ToolMan
+    func arrestCriminals(){
+    }
+}
+
+//3.
+protocol ToolMan{
+    func fixComputer()
+}
+
+//5.
+struct Engineer: ToolMan{
+    func fixComputer(){
+        }
+}
+
+let nameofsteven = Person(name: "Steven", toolMan: Engineer())
+
